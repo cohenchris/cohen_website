7069993d16613993f426d7e56aaf8ca383bf2b37
cohenchris <chris@chriscohen.dev>
2019-12-12 17:15:56 -0500

    updated slider to not fill entire screen

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Navbar, Container, Col } from "react-bootstrap";
+import { Nav, Navbar, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./NavigationBar.css";
 

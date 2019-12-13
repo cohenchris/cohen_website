dc59f7e6d8c8abd30352c660e63a06bb3f24d3ad
cohenchris <chris@chriscohen.dev>
2019-12-12 19:48:47 -0500

    added projects page, tweaked home page

--- src/main_pages/About/About.js
+++ src/main_pages/About/About.js
@@ -1,12 +1,18 @@
 import React from "react";
 import NavigationBar from "../../components/NavigationBar/NavigationBar.js";
+import ChicagoNavyPier from "../../images/ChicagoNavyPierCropped.jpg";
 
 export default class About extends React.Component {
   render() {
     return (
       <div>
         <NavigationBar />
-        <h1>About</h1>
+        <img
+          width="392p"
+          height="595.2"
+          src={ChicagoNavyPier}
+          alt="Me in Chicago"
+        />
       </div>
     );
   }
--- src/main_pages/Home/Home.js
+++ src/main_pages/Home/Home.js
@@ -4,33 +4,28 @@ import withAutoplay from "react-awesome-slider/dist/autoplay";
 import "react-awesome-slider/dist/styles.css";
 import { Container } from "react-bootstrap";
 
-import SkyDeckCora from "../../images/SkyDeckCora.jpg";
 import Skydiving1 from "../../images/Skydiving1.jpg";
-import ChicagoNavyPier from "../../images/ChicagoNavyPier.jpg";
 import ColoradoPanorama from "../../images/ColoradoPanorama.jpg";
 import MountainPicture from "../../images/MountainPicture.jpg";
-import Skydiving2 from "../../images/Skydiving2.jpg";
 import NavigationBar from "../../components/NavigationBar/NavigationBar.js";
-
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <NavigationBar />
+        <br />
+        <br />
         <Container>
           <AutoplaySlider
             play={true}
             cancelOnInteraction={false} // should stop playing on user interaction
             interval={3000}
-            fillParent={false}
+            bullets={true}
           >
             <div data-src={Skydiving1} />
-            <div data-src={SkyDeckCora} />
-            <div data-src={ChicagoNavyPier} />
             <div data-src={ColoradoPanorama} />
             <div data-src={MountainPicture} />
-            <div data-src={Skydiving2} />
           </AutoplaySlider>
         </Container>
       </div>

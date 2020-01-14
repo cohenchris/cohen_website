3525ac9efaf653e871cee18c0ed483d5f83275da
cohenchris <chris@chriscohen.dev>
2020-01-14 13:20:07 -0500

    updated website update script

--- update_website.sh
+++ update_website.sh
@@ -2,6 +2,7 @@ npm run build
 rm -rf ../cohenchris.github.io/*
 mv ./build/* ../cohenchris.github.io
 cp ./CNAME ../cohenchris.github.io/CNAME
+rm -rf ./build
 cd ../cohenchris.github.io
 curr_date=$(date +"%m-%d-%Y_%T")
 git add .

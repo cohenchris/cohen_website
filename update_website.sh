d0e0b380ac74ac4c531acd1d6efcaee7ed3d050e
cohenchris <cohen50@purdue.edu>
2020-05-11 22:10:15 -0400

    updated update_website script

--- update_website.sh
+++ update_website.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 npm run build
+if ! [[ -d ../cohenchris.github.io ]]; then
+    cd ..
+    git clone https://github.com/cohenchris/cohenchris.github.io.git
+    cd ..
+fi
+
 rm -rf ../cohenchris.github.io/*
 mv ./build/* ../cohenchris.github.io
 cp ./CNAME ../cohenchris.github.io/CNAME

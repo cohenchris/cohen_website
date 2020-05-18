1c97d712d67721769ef990ad594acfc662536921
cohenchris <chris@chriscohen.dev>
2020-05-17 22:05:55 -0400

    expanded projects, added toast to tell users that you can click on tiles

--- src/main_pages/Projects/Projects.js
+++ src/main_pages/Projects/Projects.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../../components/NavigationBar/NavigationBar.js";
 import Footer from "../../components/Footer/Footer.js";
 import ProjectCard from "./ProjectCard.js";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Toast } from "react-bootstrap";
 import "./Projects.css";
 import "../../index.css";
 import ProjectList from "./ProjectList.js";
 
 export default function Projects() {
+  const [showToast, setShowToast] = useState(true);
+
+  const closeToast = () => setShowToast(false);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -17,6 +21,12 @@ export default function Projects() {
       <NavigationBar />
 
       <div className="projectsStyle">
+        <Toast show={showToast} onClose={closeToast}>
+          <Toast.Header>
+            <strong>Click on a tile to learn more about the project!</strong>
+          </Toast.Header>
+        </Toast>
+
         <Container fluid>
           <Row>
             {ProjectList.map((project) => (
@@ -24,6 +34,7 @@ export default function Projects() {
                 <ProjectCard
                   fulltitle={project.fulltitle}
                   title={project.title}
+                  subtitle={project.subtitle}
                   desc={project.desc}
                   body={project.body}
                   timestamp={project.timestamp}

a138788aaa54ef6a68d3fb59e7ca4daf182db8ef
cohenchris <cohen50@purdue.edu>
2020-05-23 13:59:01 -0400

    Cropped 'about' picture, edited deploy script to automatically pull updated resume from github and convert it into a png for the resume page

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Upload updated resume from github repository
+git clone https://github.com/cohenchris/resume.git
+rm -f ./src/files/ChrisCohen_resume.pdf
+mv resume/ChrisCohen_resume.pdf ./src/files/
+
+# Convert resume to png for display on resume page
+rm -f ./src/images/ChrisCohen_resume.png
+pdftoppm ./src/files/ChrisCohen_resume.pdf ./src/images/ChrisCohen_resume -png -singlefile
+rm -rf resume/
+
+# Create optimized build for upload
 npm run build
 if ! [[ -d ../cohenchris.github.io ]]; then
     cd ..
@@ -7,13 +18,25 @@ if ! [[ -d ../cohenchris.github.io ]]; then
     cd cohen_website
 fi
 
+# Add required files to build repository stage
 rm -rf ../cohenchris.github.io/*
 mv ./build/* ../cohenchris.github.io
 cp ./CNAME ../cohenchris.github.io/CNAME
 cp ./404.html ../cohenchris.github.io/404.html
 rm -rf ./build
 cd ../cohenchris.github.io
+
+# Commit to build repository
 curr_date=$(date +"%m-%d-%Y_%T")
 git add .
 git commit -m "website build $curr_date"
-git push
+git push origin master
+
+cd ../cohen_website
+rm -rf ../cohenchris.github.io
+
+# Commit to development repository
+read -p "Commit message for ReactJS repository: " msg
+git add .
+git commit -m "${msg}"
+git push origin master

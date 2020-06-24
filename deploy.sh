4e97a0e4516d4c87f8ca4c12f60b865bcc4e194a
cohenchris <chris@chriscohen.dev>
2020-06-24 12:41:47 -0500

    Deployed to Vultr

--- deploy.sh
+++ deploy.sh
@@ -12,28 +12,34 @@ rm -rf resume/
 
 # Create optimized build for upload
 npm run build
-if ! [[ -d ../cohenchris.github.io ]]; then
-    cd ..
-    git clone https://github.com/cohenchris/cohenchris.github.io.git
-    cd website
-fi
+
+# # Old commands for deploying to GitHub Pages
+# if ! [[ -d ../cohenchris.github.io ]]; then
+#     cd ..
+#     git clone https://github.com/cohenchris/cohenchris.github.io.git
+#     cd website
+# fi
 
 # Add required files to build repository stage
-rm -rf ../cohenchris.github.io/*
-mv ./build/* ../cohenchris.github.io
-cp ./CNAME ../cohenchris.github.io/CNAME
-cp ./404.* ../cohenchris.github.io/
+scp -r build/* chris@chriscohen.dev:/var/www/chriscohen.dev/html
 rm -rf ./build
-cd ../cohenchris.github.io
 
-# Commit to build repository
-curr_date=$(date +"%m-%d-%Y_%T")
-git add .
-git commit -m "website build $curr_date"
-git push origin master
+# # Old commands for deploying to GitHub Pages
+# rm -rf ../cohenchris.github.io/*
+# mv ./build/* ../cohenchris.github.io
+# cp ./CNAME ../cohenchris.github.io/CNAME
+# cp ./404.* ../cohenchris.github.io/
+# rm -rf ./build
+# cd ../cohenchris.github.io
 
-cd ../website
-rm -rf ../cohenchris.github.io
+# # Commit to build repository (old for deploying to GitHub Pages)
+# curr_date=$(date +"%m-%d-%Y_%T")
+# git add .
+# git commit -m "website build $curr_date"
+# git push origin master
+
+# cd ../website
+# rm -rf ../cohenchris.github.io
 
 # Commit to development repository
 read -p "Commit message for ReactJS repository: " msg
